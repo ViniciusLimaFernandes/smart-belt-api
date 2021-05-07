8a4b2ecab09770ed8e0147e787017236f3ca6b77
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2021-05-07 13:27:39 -0300

    user and product repository operations with custom queries

--- src/main/java/com/smart/belt/application/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smart.belt.application.repository;
-
-import com.smart.belt.application.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
