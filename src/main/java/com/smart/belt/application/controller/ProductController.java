ade9c2deb97fd2788e0668d2c8a8af17fa1c9cc4
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2021-05-07 13:27:57 -0300

    controller implementation

--- src/main/java/com/smart/belt/application/controller/ProductController.java
+++ src/main/java/com/smart/belt/application/controller/ProductController.java
@@ -2,14 +2,20 @@ package com.smart.belt.application.controller;
 
 import com.smart.belt.application.data.product.ProductRequestDTO;
 import com.smart.belt.application.data.product.ProductResponseDTO;
-import com.smart.belt.application.entity.Product;
+import com.smart.belt.application.data.product.TotalProductsDTO;
 import com.smart.belt.application.service.interfaces.ProductService;
+import com.smart.belt.domain.entity.Product;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Tag(name = "Product", description = "Product requests")
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
@@ -20,6 +26,7 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "List all products based on the given parameters")
     @GetMapping(value = "/list", produces = "application/json")
     public ResponseEntity<Page<ProductResponseDTO>> findAll(final Pageable pageable) {
         //params: page, size e sort
@@ -30,6 +37,7 @@ public class ProductController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Register a new product for an user")
     @PostMapping(value = "/register", consumes = "application/json")
     public ResponseEntity<ProductResponseDTO> insertProduct(@RequestBody final ProductRequestDTO productRequestDTO) {
         final Product product = productService.create(ProductRequestDTO.convertToEntity(productRequestDTO));
@@ -37,6 +45,22 @@ public class ProductController {
         final ProductResponseDTO response = ProductResponseDTO.convertToDTO(product);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "List all products by user")
+    @GetMapping(value = "/find")
+    public ResponseEntity<List<Product>> productsByUser(@RequestParam("userID") final String userID) {
+        final List<Product> response = productService.findByUserId(userID);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Products summary by user")
+    @GetMapping(value = "/summary")
+    public ResponseEntity<List<TotalProductsDTO>> totalProductsByUserAndCondition(@RequestParam("userID") final String userID) {
+        final List<TotalProductsDTO> response = productService.total(userID);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/smart/belt/application/controller/UserController.java
+++ src/main/java/com/smart/belt/application/controller/UserController.java
@@ -3,13 +3,16 @@ package com.smart.belt.application.controller;
 import com.smart.belt.application.data.user.UserRequestDTO;
 import com.smart.belt.application.data.user.UserResponseDTO;
 import com.smart.belt.application.service.interfaces.UserService;
+import com.smart.belt.domain.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User", description = "User requests")
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -20,10 +23,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Create a new user")
     @PostMapping(value = "/create", consumes = "application/json")
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody final UserRequestDTO userRequestDTO) {
         final UserResponseDTO response = userService.createUser(userRequestDTO);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "List all users")
+    @GetMapping(value = "/list", produces = "application/json")
+    public ResponseEntity<Page<UserResponseDTO>> findAll(final Pageable pageable) {
+        //params: page, size e sort
+        final Page<User> userPage = userService.listAll(pageable);
+
+        final Page<UserResponseDTO> response = userPage.map(UserResponseDTO::convertToResponse);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
